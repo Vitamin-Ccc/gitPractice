43f5dc127677a7cee9ba25340679cf044de557d4
Sky Lin <skylin@skys-mbp.uconnect.utah.edu>
2021-11-12 11:17:02 -0700

    some dummy data

--- demo.rb
+++ demo.rb
@@ -1,3 +1,16 @@
 puts "hello world"
 
-puts "1 + 1 = 2"
+puts "1 + 1 = 2
+puts "Hello from vim"
+
+# setup do this for each project
+# git init to create a repo
+# create a repo on github
+# add the remote link
+
+
+# basic flow
+# make change in your code
+# add(stage) those changes git add .  or git add filename
+# commit those changes git commit -m'description of what you did'
+# push up to github
