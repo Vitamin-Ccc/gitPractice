365b4b103147dffdab674631f008541e5819c2ba
Sky Lin <skylin@skys-mbp.uconnect.utah.edu>
2021-11-12 12:08:03 -0700

    git steps

--- demo.rb
+++ demo.rb
@@ -4,13 +4,22 @@ puts "1 + 1 = 2
 puts "Hello from vim"
 
 # setup do this for each project
-# git init to create a repo
+# git init to create a repo (rails does this for you)
 # create a repo on github
-# add the remote link
+# add the remote link(remember to use the ssh link on github)
 
 
 # basic flow
 # make change in your code
 # add(stage) those changes git add .  or git add filename
 # commit those changes git commit -m'description of what you did'
-# push up to github
+
+# pull from github(if needed)
+# fix any conflicts if they exist
+# when needed, push up to github. don't have to do it every 2 minutes
+
+# if working with a partner
+# one of you will create the repo, the others will clone down repo(once)
+
+# when a team member pushes to master, they should tell everyone to pull from master (ie git pull origin master)
+# pull from github
